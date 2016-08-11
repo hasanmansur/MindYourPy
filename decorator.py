357fe61300ad8674ca729c8de8c90e5d34d546ed
smhasanmansur <smhasanmansur@gmail.com>
2016-08-11 14:44:24 +0600

    method decorator

--- decorator.py
+++ decorator.py
@@ -59,3 +59,53 @@ def hello(name):
     return 'hello ' + name
 
 print(hello('hasan'))
+
+
+print('--------------------------------------Decorating Methods------------------------------------------')
+
+'''
+In Python, methods are functions that expect their first parameter to be a reference to the current object. 
+We can build decorators for methods the same way, while taking self into consideration in the wrapper function.
+'''
+
+def p_decorate(func):
+    def func_wrapper(self):
+        return '<p>{0}</p>'.format(func(self))
+    return func_wrapper
+
+class Person():
+    def __init__(self):
+        self.name = 'John'
+        self.family = 'Doe'
+    
+    @p_decorate
+    def get_name(self):
+        return self.name + ' ' + self.family
+
+test_person = Person()
+print(test_person.get_name())
+
+'''
+A much better approach would be to make our decorator useful for functions and methods alike. 
+This can be done by putting *args and **kwargs as parameters for the wrapper, then it can accept 
+any arbitrary number of arguments and keyword arguments.
+'''
+
+def p_decorate(func):
+    def func_wrapper(*args, **kwargs):
+        return '<p>{0}</p>'.format(func(*args, **kwargs))
+    return func_wrapper
+
+class Person():
+    def __init__(self):
+        self.name = 'John'
+        self.family = 'Doe'
+    
+    @p_decorate
+    def get_name(self):
+        return self.name + ' ' + self.family
+
+test_person = Person()
+print(test_person.get_name())
+
+
